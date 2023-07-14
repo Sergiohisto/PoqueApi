c6c5f171131b85eb608bec5c1c11a1c69cc643a6
Sergio Luis Arrieta <slarrieta02@gmail.com>
2023-07-14 12:20:32 -0500

    Confijuracion de la API para lectura de Pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,38 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
-  let [number, setNumber] = useState(10);
+  let [pokemonNumber, setPokemonNumber] = useState(10);
+  let [pokemonName, setPokemonName] = useState("");
 
   function increaseNumber() {
-    setNumber(number + 1);
-    console.log("valor antes del nuevo render:" + number);
+    setPokemonNumber(pokemonNumber + 1);
+    console.log("valor antes del nuevo render:" + pokemonNumber);
   }
 
   useEffect(() => {
-    console.log("valor al actualizar el estado:" + number);
-  });
+    console.log("valor al actualizar el estado:" + pokemonNumber);
+    // aqui debemos llamar al API
+    //fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber}/`)
+    //.then((result) => result.json())
+    //.then((data) => setPokemonName(data.name));
+
+    searchPokemon(pokemonNumber);
+  }, [pokemonNumber]);
+
+  let searchPokemon = async (pokemonNumber) => {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonNumber}/`
+    );
+    const data = await response.json();
+    setPokemonName(data.name);
+  };
+
   return (
     <>
       <button onClick={increaseNumber}>Next</button>
-      <div>{number}</div>
+      <div>
+        {pokemonNumber} - {pokemonName}{" "}
+      </div>
     </>
   );
 };
